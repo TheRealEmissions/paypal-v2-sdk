ed8ad89cf4216c03c5349dba1c6fc7154af8ebde
Emissions <unitedrealm@hotmail.com>
2022-05-07 11:41:09 +0100

    logging

--- src/API/Authentication/Token.js
+++ src/API/Authentication/Token.js
@@ -19,7 +19,7 @@ class Token {
         responseType: "json",
       }
     );
-    console.log(JSON.stringify(response));
+    console.log(JSON.stringify(response.data));
     this.setAccessToken(response.data.access_token)
       .setAppId(response.data.app_id)
       .setExpiry(response.data.expires_in)
